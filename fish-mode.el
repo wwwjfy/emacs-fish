55ab673e6014c10d1b5669720afc12228587de8d
Tony Wang <wwwjfy@gmail.com>
2013-08-22 23:49:54 +0800

    highlight quotes and variables

--- fish-mode.el
+++ fish-mode.el
@@ -7,12 +7,15 @@
 ir[hs]\\|e\\(?:cho\\|lse\\|mit\\|nd\\|val\\|x\\(?:ec\\|it\\)\\)\\|f\\(?:g\\|ish\\(?:_\\(?:config\\|indent\\|p\\(?:ager\\|rompt\\)\\|right_prompt\\|update_completions\\)\\|d\\)?\\|or\\|\
 unc\\(?:ed\\|save\\|tions?\\)\\)\\|h\\(?:elp\\|istory\\)\\|i\\(?:f\\|satty\\)\\|jobs\\|m\\(?:ath\\|imedb\\)\\|n\\(?:extd\\|ot\\)\\|o\\(?:pen\\|r\\)\\|p\\(?:opd\\|revd\\|sub\\|\\(?:ush\\
 \|w\\)d\\)\\|r\\(?:andom\\|e\\(?:ad\\|turn\\)\\)\\|s\\(?:et\\(?:_color\\)?\\|ource\\|tatus\\|witch\\)\\|t\\(?:est\\|rap\\|ype\\)\\|u\\(?:limit\\|mask\\)\\|vared\\|while\\)\\>"
-    . font-lock-builtin-face)))
+    . font-lock-builtin-face)
+   '("\\$\\([[:alpha:]_][[:alnum:]_]*\\)" . font-lock-variable-name-face)))
 
 (defvar fish-mode-syntax-table
   (let ((fish-mode-syntax-table (make-syntax-table)))
     (modify-syntax-entry ?# "<")
-    (modify-syntax-entry ?\n ">")))
+    (modify-syntax-entry ?\n ">")
+    (modify-syntax-entry ?\" "\"\"")
+    (modify-syntax-entry ?\' "\"'")))
 
 (defun fish-indent-line ()
   "Indent current line."
